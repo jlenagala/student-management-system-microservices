3f3204ee44752571af7e8476cf8bad4fbc0c8600
jlenagala <jananianut@gmail.com>
2020-03-06 06:00:06 +0530

    Initial Commit-student-service

---
+++ student-service/src/main/java/com/janani/sms/studentservice/model/Student.java
@@ -0,0 +1,14 @@
+package com.janani.sms.studentservice.model;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    int id;
+    String firstName;
+    String lastName;
+    int age
+    LocalDate regDate;
+
+
+}
